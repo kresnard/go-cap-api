b60b3aae4bbe351b2e4082167d1183b0f7a6cc05
Kresna Rangga Darmansyah <kresnard@gmail.com>
2022-07-27 11:39:57 +0700

    implementasi Hexagonal Architecture

--- app/app.go
+++ app/app.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"capi/domain"
+	"capi/service"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,18 +12,21 @@ func Start() {
 
 	mux := mux.NewRouter()
 
+	// wiring
+	ch := CustomerHandler{service.NewCustomerService(domain.NewCustomerRepositoryStub())}
+
 	// mux := http.NewServeMux()
 
 	// * defining routes
 	// mux.HandleFunc("/greet", greet)
 	// mux.HandleFunc("/customer", getCustomers)
-	mux.HandleFunc("/greet", greet).Methods(http.MethodGet)
-	mux.HandleFunc("/customer", getAllCustomers).Methods(http.MethodGet)
-	mux.HandleFunc("/customer", addCustomer).Methods(http.MethodPost)
+	// mux.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	mux.HandleFunc("/customer", ch.getAllCustomers).Methods(http.MethodGet)
+	// mux.HandleFunc("/customer", addCustomer).Methods(http.MethodPost)
 	// mux.HandleFunc("/customer/{customer_id}", getCustomers).Methods(http.MethodGet)
-	mux.HandleFunc("/customer/{customer_id:[0-9]+}", getCustomers).Methods(http.MethodGet)
-	mux.HandleFunc("/customer/{customer_id:[0-9]+}", updateCustomers).Methods(http.MethodPut)
-	mux.HandleFunc("/customer/{customer_id:[0-9]+}", deleteCustomers).Methods(http.MethodDelete)
+	// mux.HandleFunc("/customer/{customer_id:[0-9]+}", getCustomers).Methods(http.MethodGet)
+	// mux.HandleFunc("/customer/{customer_id:[0-9]+}", updateCustomers).Methods(http.MethodPut)
+	// mux.HandleFunc("/customer/{customer_id:[0-9]+}", deleteCustomers).Methods(http.MethodDelete)
 
 	// * starting the server
 	http.ListenAndServe(":8080", mux)
--- app/handler.go
+++ app/handler.go
@@ -1,33 +1,37 @@
 package app
 
 import (
+	"capi/service"
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/gorilla/mux"
+	// "github.com/go-delve/delve/service"
 )
 
-type Customer struct {
-	ID      int    `json:"id" xml:"id"`
-	Name    string `json:"name" xml:"name"`
-	City    string `json:"city" xml:"city"`
-	ZipCode string `json:"zip_code" xml:"zipcode"`
+// type Customer struct {
+// 	ID      int    `json:"id" xml:"id"`
+// 	Name    string `json:"name" xml:"name"`
+// 	City    string `json:"city" xml:"city"`
+// 	ZipCode string `json:"zip_code" xml:"zipcode"`
+// }
+
+// var customers []Customer = []Customer{
+// 	{1, "User1", "Jakarta", "123456"},
+// 	{2, "User2", "Surabaya", "67890"},
+// }
+
+// func greet(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "Hello Celerates!")
+// }
+
+type CustomerHandler struct {
+	service service.CustomerService
 }
 
-var customers []Customer = []Customer{
-	{1, "User1", "Jakarta", "123456"},
-	{2, "User2", "Surabaya", "67890"},
-}
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello Celerates!")
-}
-
-func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+func (ch *CustomerHandler) getAllCustomers(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprint(w, "get customerendpoint")
+
+	customers, _ := ch.service.GetAllCustomer()
 
 	if r.Header.Get("Content-Type") == "application/xml" {
 		w.Header().Add("Content-Type", "application/xml")
@@ -38,137 +42,137 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getCustomers(w http.ResponseWriter, r *http.Request) {
+// func getCustomers(w http.ResponseWriter, r *http.Request) {
 
-	// get route variable
-	vars := mux.Vars(r)
+// 	// get route variable
+// 	vars := mux.Vars(r)
 
-	customerId := vars["customer_id"]
+// 	customerId := vars["customer_id"]
 
-	// convert str to int
-	// id, _ := strconv.Atoi(customerId)
-	id, err := strconv.Atoi(customerId)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "invalid customer id")
-	}
+// 	// convert str to int
+// 	// id, _ := strconv.Atoi(customerId)
+// 	id, err := strconv.Atoi(customerId)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 		fmt.Fprint(w, "invalid customer id")
+// 	}
 
-	// searching customer data
-	var cust Customer
+// 	// searching customer data
+// 	var cust Customer
 
-	for _, data := range customers {
-		if data.ID == id {
-			cust = data
-		}
-	}
+// 	for _, data := range customers {
+// 		if data.ID == id {
+// 			cust = data
+// 		}
+// 	}
 
-	// if cust.ID == 0 {
-	// 	w.WriteHeader(http.StatusNotFound)
-	// 	fmt.Fprint(w, "customer data not found")
-	// 	return
-	// }
+// 	// if cust.ID == 0 {
+// 	// 	w.WriteHeader(http.StatusNotFound)
+// 	// 	fmt.Fprint(w, "customer data not found")
+// 	// 	return
+// 	// }
 
-	// return customer datagit
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(cust)
+// 	// return customer datagit
+// 	w.Header().Add("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(cust)
 
-}
+// }
 
-func addCustomer(w http.ResponseWriter, r *http.Request) {
-	// decode request body
-	var cust Customer
-	json.NewDecoder(r.Body).Decode(&cust)
+// func addCustomer(w http.ResponseWriter, r *http.Request) {
+// 	// decode request body
+// 	var cust Customer
+// 	json.NewDecoder(r.Body).Decode(&cust)
 
-	// generate new id
-	nextID := getNextID()
-	cust.ID = nextID
+// 	// generate new id
+// 	nextID := getNextID()
+// 	cust.ID = nextID
 
-	// save data to array
-	customers = append(customers, cust)
+// 	// save data to array
+// 	customers = append(customers, cust)
 
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintln(w, "customer succesfully created")
+// 	w.WriteHeader(http.StatusCreated)
+// 	fmt.Fprintln(w, "customer succesfully created")
 
-	// fmt.Println(cust)
-}
+// 	// fmt.Println(cust)
+// }
 
-func getNextID() int {
-	cust := customers[len(customers)-1]
+// func getNextID() int {
+// 	cust := customers[len(customers)-1]
 
-	return cust.ID + 1
-}
+// 	return cust.ID + 1
+// }
 
-func updateCustomers(w http.ResponseWriter, r *http.Request) {
-	// get route variable
-	vars := mux.Vars(r)
+// func updateCustomers(w http.ResponseWriter, r *http.Request) {
+// 	// get route variable
+// 	vars := mux.Vars(r)
 
-	customerId := vars["customer_id"]
+// 	customerId := vars["customer_id"]
 
-	// convert str to int
-	// id, _ := strconv.Atoi(customerId)
-	id, err := strconv.Atoi(customerId)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "invalid customer id")
-	}
+// 	// convert str to int
+// 	// id, _ := strconv.Atoi(customerId)
+// 	id, err := strconv.Atoi(customerId)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 		fmt.Fprint(w, "invalid customer id")
+// 	}
 
-	// searching customer data
-	var cust Customer
+// 	// searching customer data
+// 	var cust Customer
 
-	for customerIndex, data := range customers {
-		if data.ID == id {
+// 	for customerIndex, data := range customers {
+// 		if data.ID == id {
 
-			// save temp data for validation
-			cust = data
+// 			// save temp data for validation
+// 			cust = data
 
-			// decode request body
-			var newCust Customer
-			json.NewDecoder(r.Body).Decode(&newCust)
+// 			// decode request body
+// 			var newCust Customer
+// 			json.NewDecoder(r.Body).Decode(&newCust)
 
-			// do update
-			customers[customerIndex].Name = newCust.Name
-			customers[customerIndex].City = newCust.City
-			customers[customerIndex].ZipCode = newCust.ZipCode
+// 			// do update
+// 			customers[customerIndex].Name = newCust.Name
+// 			customers[customerIndex].City = newCust.City
+// 			customers[customerIndex].ZipCode = newCust.ZipCode
 
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprint(w, "customer data updated")
+// 			w.WriteHeader(http.StatusOK)
+// 			fmt.Fprint(w, "customer data updated")
 
-		}
-	}
-	if cust.ID == 0 {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "customer data not found")
-		return
-	}
+// 		}
+// 	}
+// 	if cust.ID == 0 {
+// 		w.WriteHeader(http.StatusNotFound)
+// 		fmt.Fprint(w, "customer data not found")
+// 		return
+// 	}
 
-}
+// }
 
-func deleteCustomers(w http.ResponseWriter, r *http.Request) {
-	// get route variable
-	vars := mux.Vars(r)
-	customerId := vars["customer_id"]
+// func deleteCustomers(w http.ResponseWriter, r *http.Request) {
+// 	// get route variable
+// 	vars := mux.Vars(r)
+// 	customerId := vars["customer_id"]
 
-	// convert str to int
-	id, _ := strconv.Atoi(customerId)
-	// id, err := strconv.Atoi(customerId)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	fmt.Fprint(w, "invalid customer id")
-	// }
+// 	// convert str to int
+// 	id, _ := strconv.Atoi(customerId)
+// 	// id, err := strconv.Atoi(customerId)
+// 	// if err != nil {
+// 	// 	w.WriteHeader(http.StatusBadRequest)
+// 	// 	fmt.Fprint(w, "invalid customer id")
+// 	// }
 
-	// searching customer data
-	var updateCustomers Customer
-	json.NewDecoder(r.Body).Decode(&updateCustomers)
+// 	// searching customer data
+// 	var updateCustomers Customer
+// 	json.NewDecoder(r.Body).Decode(&updateCustomers)
 
-	for i, cust := range customers {
-		if cust.ID == id {
-			customers = append(customers[:i], customers[i+1:]...)
-			// customers = append(customers, updateCustomers)
-		}
-	}
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "customer data deleted")
-}
+// 	for i, cust := range customers {
+// 		if cust.ID == id {
+// 			customers = append(customers[:i], customers[i+1:]...)
+// 			// customers = append(customers, updateCustomers)
+// 		}
+// 	}
+// 	w.WriteHeader(http.StatusOK)
+// 	fmt.Fprint(w, "customer data deleted")
+// }
 
 // func updateCustomers(w http.ResponseWriter, r *http.Request) {
 // 	// get route variable
